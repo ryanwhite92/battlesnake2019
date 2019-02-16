228a79c2155a6a277525dbce40d470e3a6915853
Ryan White <ryanjwhite10@gmail.com>
2019-02-15 16:53:14 -0800

    Refactor getPossibleMoves function

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(poweredByHandler)
 function calcMove({ board, you }) {
   const snakeHead = you.body[0];
 
-  const possibleMoves = getPossibleMoves(snakeHead, board, you);
+  const possibleMoves = getPossibleMoves(board, you);
   const target = findShortestPath(snakeHead, board.food);               // find closest food node
   const nextMove = findShortestPath(target, possibleMoves);             // find move resulting in shortest path to get to target node
   
@@ -37,37 +37,52 @@ function calcMove({ board, you }) {
   if (yDir < 0) return 'down';
 }
 
-function getPossibleMoves(snakeHead, board, you) {
-  const { width, height, snakes } = board;
-  const otherSnakes = snakes.filter((snake) => {
-    if (!util.isDeepStrictEqual(you.body, snake.body)) return snake;
-  });
+function samePoint(pointA, pointB) {
+  return pointA.x === pointB.x && pointA.y === pointB.y;
+}
 
+function getPossibleDirections({ body }) {
+  const snakeHead = body[0];
   const moves = [
     { x: snakeHead.x, y: snakeHead.y + 1 },
     { x: snakeHead.x, y: snakeHead.y - 1 },
     { x: snakeHead.x + 1, y: snakeHead.y },
     { x: snakeHead.x - 1, y: snakeHead.y },
   ];
+  return moves;
+}
+
+function getOtherSnakes({ snakes }, { body: self}) {
+  return snakes.filter((snake) => {
+    if (!util.isDeepStrictEqual(self, snake.body)) return snake;
+  });
+}
+
+function getPossibleMoves(board, you) {
+  const { width, height } = board;
+  const otherSnakes = getOtherSnakes(board, you);
+  const moves = getPossibleDirections(you);
   
   return moves.filter((move) => {
     // Remove moves that fall outside of board boundaries
     if (move.x < 0 || move.x >= width || move.y < 0 || move.y >= height) {
       return false;
     }
-    // Remove moves that would result in self-collisions
+
+    // // Remove moves that would result in self-collisions
     for (let i = 1; i < you.body.length; i++) {
-      if (move.x == you.body[i].x && move.y == you.body[i].y) {
+      let isSamePoint = samePoint(move, you.body[i]);
+      if (isSamePoint) {
         return false;
       }
     }
+
     // Remove moves that would result in collisions with other snakes
-    if (otherSnakes.length > 0) {
-      for (let i = 0; i < otherSnakes.length; i++) {
-        for (let j = 0; j < otherSnakes[i].body.length; j++) {
-          if (move.x == otherSnakes[i].body[j].x && move.y == otherSnakes[i].body[j].y) {
-            return false;
-          }
+    for (let i = 0; i < otherSnakes.length; i++) {
+      for (let j = 0; j < otherSnakes[i].body.length; j++) {
+        let isSamePoint = samePoint(move, otherSnakes[i].body[j]);
+        if (isSamePoint) {
+          return false;
         }
       }
     }
